af6169c73eb6edc62a38e61840efcb28e94e6e1a
A624\1DAW <marcanlop05@gmail.com>
2024-09-25 21:19:06 +0200

    Ejercicio 4 a mitad

--- Ejercicios/src/main/java/org/example/Ejercicios.java
+++ Ejercicios/src/main/java/org/example/Ejercicios.java
@@ -25,4 +25,83 @@ public class Ejercicios {
         System.out.println("La suma de los numero es:" + resultado);
 
     }
+
+    public void ejercicio_1(){
+        System.out.println("Dame un numero");
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+        if ( numero > 0 ) {
+            System.out.println("El numero es positivo");}
+        else{
+            if (numero==0){
+                System.out.println("Este numero es neutro");
+            }
+            else {
+                System.out.println("Este numero es negativo");
+            }
+        }
+
+    }
+
+    public void ejercicio_2(){
+        System.out.println("Dame un numero");
+        Scanner entrada = new Scanner(System.in);
+        int num1 = entrada.nextInt();
+        System.out.println("Dame otro numero");
+        int num2 = entrada.nextInt();
+        if (num1 != num2 ){
+            if (num1 > num2){
+                System.out.println("El " + num1 + "es mayor que " + num2);}
+            else  {
+                System.out.println("El " + num2 + "es mayor que " + num1);}
+            }
+        else {
+            System.out.println("ERROR:los numeros introducidos son iguales");
+        }
+
+    }
+
+    public void ejercicio_3(){
+        System.out.println("Dame un numero");
+        Scanner entrada = new Scanner(System.in);
+        int num1 = entrada.nextInt();
+        System.out.println("Dame otro numero");
+        int num2 = entrada.nextInt();
+        if(num2 == 0 ){
+            System.out.println("ERROR no se puede dividir entre 0");
+        }
+        else{
+            int resultado = num1 / num2;
+            System.out.println(+ num1 + " entre " + num2 + " es igual a " + resultado);
+
+        }
+    }
+
+    public void ejercicio_4() {
+        System.out.println("Dame la base");
+        Scanner entrada = new Scanner(System.in);
+        int num1 = entrada.nextInt();
+        if (num1 < 0) {
+            System.out.println("El valor tiene que ser positivo");
+        } else {
+            if (num1 == 0) {
+                System.out.println("El valor tiene que ser mayor a 0");
+            } else {
+                System.out.println("Dame la altura");
+                int num2 = entrada.nextInt();
+                if (num2 > 0) {
+                    System.out.println("El valor tiene que ser positivo");
+                } else {
+                    if (num2 == 0) {
+                        System.out.println("El valor tiene que ser mayor a 0");
+                    } else {
+                        int resultado1 = num1 * num2;
+                        int resultado2 = resultado1 / 2;
+                        System.out.println("El area del triangulo es" + resultado2);
+                    }
+
+                }
+            }
+        }
+    }
 }
