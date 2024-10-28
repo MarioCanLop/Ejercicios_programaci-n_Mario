cdc6b97a1fbbc9575ee4c27c274375504673f395
DESKTOP-RMOGN5O\Mario <marcanlop05@gmail.com>
2024-10-28 11:37:26 +0100

    si

--- Ejercicios/src/main/java/org/example/Main.java
+++ Ejercicios/src/main/java/org/example/Main.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import Ejercicios.src.main.java.org.example.Repaso;
+
 import java.io.PrintStream;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+//public class Main {
+    //public static void main(String[] args) {
 
-        Ejercicios ejercicio = new Ejercicios();
-        ejercicio.Ejercicio2_bat1();
+        //Ejercicios ejercicio = new Ejercicios();
+        //ejercicio.Ejercicio2_bat1();
+    //}
+//}
+public class Main {
+    public static void main(String[] args){
+        Repaso repaso = new Repaso();
+        repaso.repaso3();
     }
 }
